b78a58121b451b909078928b0aa5194cf945a190
Riccardo <riccafiore@tiscali.it>
2018-06-03 00:55:25 +0200

    aggiunta trackApi, aggiornati e aggiunti file per utilizzarla,modifiche al css

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,11 @@
 const msg_q = require("./amqp_functions");
+var configAuth = require('../config/auth');
 
 module.exports = function(app, passport, path , express, amqp) {
+
+    var id_slugs = [];
+    var title = [];
+    var year = [];
 
 
     // HOME PAGE ===========================
@@ -88,6 +93,77 @@ module.exports = function(app, passport, path , express, amqp) {
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
+    });
+
+
+    app.get('/movies', isLoggedIn, function(req, res) {    // we will check if an user is loggedin by using route middleware (isLoggedIn function)
+        var request = require('request');
+        request({
+            headers: {
+                'Content-Type': configAuth.traktApi.ContentType,
+                'trakt-api-version': configAuth.traktApi.ApiVersion,
+                'trakt-api-key': configAuth.traktApi.clientID
+            },
+            url: 'https://api.trakt.tv/movies/boxoffice/'//URL to hit
+        }, function (error, response, body) {
+            if(error) {
+                console.log(error);
+            } else {
+                var body1 = JSON.parse(body);
+                var i;
+                for (i = 0; i < 5; i++){
+                    var mov = JSON.stringify(body1[i].movie);
+                    title[i] = JSON.parse(mov).title;
+                    var id = JSON.parse(mov).ids;
+                    id = JSON.stringify(id);
+                    console.log(id);
+                    id_slugs[i] = JSON.parse(id).slug;
+                    year[i] = JSON.parse(mov).year;
+                }
+                console.log(id_slugs[0]);
+                res.render('cinema.ejs', {
+                    user : req.user,
+                    title : title,
+                    year  : year,
+                    ids: id_slugs
+                });
+            }
+        });
+
+    });
+
+
+    app.post('/movies/ratings', isLoggedIn, function(req, res) {
+        var request = require('request');
+        var rat = [];
+        var movie = req.body.movie;
+        console.log(movie);
+        request({
+            url: 'https://api.trakt.tv/movies/' + id_slugs[movie] + '/ratings',
+            headers: {
+                'Content-Type': 'application/json',
+                'trakt-api-version': '2',
+                'trakt-api-key': '05d8b28cffe1cdaff3a23f04a5e26970d566208439729874c1c5eccdb6bf9de7'
+            }
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                var body1 = JSON.parse(body);
+                var distribution = JSON.stringify(body1.distribution);
+                console.log(distribution);
+                var j;
+                for (j = 0; j < 10; j++) {
+                    rat[j] = JSON.parse(distribution)[j + 1];
+                    console.log(rat[j]);
+                }
+                res.render('ratings.ejs', {
+                    user : req.user,
+                    title : title[movie],
+                    rat: rat
+                });
+            }
+        });
     });
 
 };
--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,14 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
+
+    'traktApi' : {
+        'clientID'      : '05d8b28cffe1cdaff3a23f04a5e26970d566208439729874c1c5eccdb6bf9de7', // your App ID
+        'clientSecret'  : '3f17301b35af37a5018197063a8df791828b664f8767aeb4255d86cd255be0b7', // your App Secret
+        'ContentType'  : 'application/json',
+        'ApiVersion'   : '2'
+    },
+
     'facebookAuth' : {
         'clientID'      : '1733208910318408', // your App ID
         'clientSecret'  : 'b90724c3ef76b8027837c9633148e75f', // your App Secret
